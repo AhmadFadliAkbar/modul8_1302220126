7417f03bd08ac49b8f9a66c9203c91b11e046b7c
AhmadFadliAkbar <135632054+AhmadFadliAkbar@users.noreply.github.com>
2024-04-17 08:16:37 +0700

    add

---
+++ BankTransferConfig.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text.Json;
+
+
+	public class BankTransferConfig
+	{
+        public int nominal_transfer { get; set; }
+        public int biaya_transfer { get; set; }
+        public int total_biaya { get; set;  }
+        public string CONFIG1 { get; set; }
+        public int CONFIG2 { get; set; }
+        public int CONFIG3 { get; set; }
+        public int CONFIG4 { get; set; }
+        public string CONFIG5 { get; set; }
+        public string CONFIG6 { get; set; }
+        public string CONFIG7 { get; set; }
+
+
+
+        public BankTransferConfig() {}
+	}
+
+    public class AplikasiConfig {
+        public BankTransferConfig Config;
+        private const string filePath = "bank_transfer_config.json";
+
+        public AplikasiConfig() {
+            try
+            {
+                ReadConfigJson();
+            }
+            catch {
+                Default();
+                PrintConfig();
+            }
+        }
+        public void Default()
+        {
+            Config = new BankTransferConfig();
+
+            Config.CONFIG1 = “en”;
+            Config.CONFIG2 = 25000000;
+            Config.CONFIG3 = 6500;
+            Config.CONFIG4 = 15000;
+            Config.CONFIG5 = [ “RTO(real - time)”, “SKN”, “RTGS”, “BI FAST” ];
+            Config.CONFIG6 = “yes”;
+            Config.CONFIG7 = “ya”;
+        }
+        public void ReadConfigJson() {
+            string configDataJson = File.ReadAllText(filePath);
+            Config = JsonSerializer.Deserialize<BankTransferConfig>(configDataJson);
+
+        }
+        public void PrintConfig() {
+            JsonSerializerOptions Option = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            string output = JsonSerializer.Serialize(Config);
+            File.WriteAllText(filePath, output);
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json;
+using static BankTransferConfig;
+
+internal class program {
+    public static void Main(string[] args) {
+        AplikasiConfig Config = new AplikasiConfig();
+
+
+        Console.WriteLine("Masukkan nominal transfer: ");
+        total_biaya = nominal_transfer + biaya transfer;
+        Console.WriteLine(total_biaya);
+
+    }
+}
